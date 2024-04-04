7ce550ed06974941a6a73ee38f56fcdf18c7b908
Jamen <jamen.lang@gmail.com>
2024-04-04 10:54:51 -0600

    Update scripted_call.py

--- examples/scripted_call.py
+++ examples/scripted_call.py
@@ -1,28 +1,41 @@
+import sys
 from baresipy import BareSIP
 from time import sleep
+import signal
 
+to = "7157"
 
-gateway = "your_sip.gateway.net"
-user = "your_phone"
-pswd = "your_password"
+gateway = "sip.gateway"
+user = "7150"
+auth_user = "testuser"
+auth_pswd = "testpassword"
 debug = False
 
-b = BareSIP(user, pswd, gateway, debug=debug)
 
-to = "jarbas_laptop@sipx.mattkeys.net"
-speech = "this is jarbas personal assistant speaking. this was a test"
-audio = "examples/acdc.mp3"
-
-
-b.call(to)
+b = BareSIP(user, auth_user, auth_pswd, gateway, debug=debug)
 
 while b.running:
-    sleep(0.5)
-    if b.call_established:
-        b.send_dtmf("123")
 
-        b.speak(speech)
-        b.speak("Goodbye")
-        # b.send_audio(audio)
+    b.call(to)
+    
+    if b.call_established:
+        b.send_audio("/usr/share/baresip/dot.mp3")
+        #b.send_dtmf("123")
+        if b.dtmf:
+            print(b.dtmf_digit)
+            if(b.dtmf_digit == 2):
+                b.send_audio("/usr/share/baresip/ack.mp3")
+            if(b.dtmf_digit == 9):
+                b.send_audio("/usr/share/baresip/review.mp3")
+
         b.hang()
         b.quit()
+    sleep(5)
+
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    b.quit()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
